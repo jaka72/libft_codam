b4814f61cb1d6dcda09cb101a55711ed49228fea
jaka72 <jaka72@gmail.com>
2021-10-22 13:07:38 +0200

    added overflow protection

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,20 @@
 /*   By: jmurovec <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/30 11:33:09 by jmurovec      #+#    #+#                 */
-/*   Updated: 2020/11/08 15:25:59 by jmurovec      ########   odam.nl         */
+/*   Updated: 2021/10/22 13:07:03 by jaka          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include "libft.h"
 
 void		*ft_calloc(size_t count, size_t size)
 {
 	char	*pt;
+
+	if (size && (SIZE_MAX / size) < count)
+		return NULL;
 
 	pt = malloc(count * size);
 	if (!pt)
